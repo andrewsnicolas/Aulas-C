6b72e91adca4fa69939ea528d5480cf0c785cde4
Andrews Nícolas <104273656+andrewsnicolas@users.noreply.github.com>
2026-03-25 11:39:01 -0300

    Mudança de extensão do arquivo cpp->c

---
+++ Ex003.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+int main() {
+
+    int menor, k = 0, num, quant;
+
+    do {
+        printf("\nQual eh a quantidade de numeros que voce ira digitar?\n");
+        scanf("%d", &k);
+    } while (k<1);
+
+    for (int c = 0; c < k; c++) {
+        printf("\nDigite o %d numero: \n", (c+1));
+        scanf("%d", &num);
+        if (c==0) menor = num;
+        if (menor>num) {
+            menor = num;
+            quant = 1;
+        }
+        else if (menor==num) quant++;
+    }
+
+    printf("\nO menor numero eh o %d e ele aparece %d vez(es)", menor, quant);
+    return 0;
+}
---
+++ Ex005.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+int main()
+{
+    int k;
+    int num[k];
+    printf("Digite a quantidade de numeros que voce ira digitar: ");
+    scanf("%d", &k);
+    for (int i = 0; i<k; i++)
+    {
+        scanf("%d", &num[i]);
+    }
+}
